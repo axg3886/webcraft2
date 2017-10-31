4023eb88e822f7b21e876954462caf33ef406040
Ashwin Ganapathiraju <ag4ce@hotmail.com>
2017-10-31 00:03:03 -0400

    In the intrest of memory, let's make the chunks smaller.

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -2275,6 +2275,7 @@ app.main = app.main || {
     this.user.torch.transform.position.elements[1] += 2;
 
     // Emit update
+    this.user.lastUpdate = new Date().getTime();
     this.genWorker.emit('movement', this.getSendingUser());
   },
   getSendingUser: function getSendingUser() {
